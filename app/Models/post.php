081c351c8f6faad99e3ce0994dc09e27fc7acbee
hofer yuan mangaoil <hoferyuanm@gmail.com>
2024-07-18 10:58:38 +0800

    laravel Model Controller

---
+++ app/Models/post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use HasFactory;
+
+    public function Users()
+    {
+
+    }
+
+    public function post_comments()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('post', post::class);
+Route::resource('comment', comment::class);
+Route::resource('user', User::class);
